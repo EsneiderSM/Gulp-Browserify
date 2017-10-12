4f465ae528ea486f060da75b7dee0921c5e4d04e
Esneider Serna <esneider.serna@doctus.com.co>
2017-10-12 17:11:38 -0500

    Added package gulp, gulp-sass and autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,32 @@
 var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
 
 // Static server
 gulp.task('browser-sync', function() {
     browserSync.init({
         server: {
-            baseDir: "./"
+            baseDir: "./app"
         }
     });
+
+    gulp.watch("app/sass/*.scss", ['sass']);
+    gulp.watch("app/*.html").on('change', browserSync.reload);
+    gulp.watch("app/js/main.js").on('change', browserSync.reload);
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    return gulp.src("app/sass/*.scss")
+    .pipe(sass({
+        outputStyle: 'expanded'
+    }))
+    .pipe(autoprefixer({
+        versions: ['last 2 browsers']
+    }))
+    .pipe(gulp.dest("app/css"))
+    .pipe(browserSync.stream());
 });
 
 gulp.task('default', ['browser-sync']);
